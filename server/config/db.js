4c764b1ee89ae709f1ccf394c924199ba4b6c211
Ash Sharma <ashrut05@protonmail.com>
2021-03-06 21:21:38 -0500

    Initial Commit

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+/**
+ * Add your code here to fix URI connection
+ *
+ */
+module.exports = 
+{
+    //URI:"mongodb://localhost/book_store"
+    "URI":"mongodb+srv://ash:lK5JCz1r7dbKy78a@mongodbserver.tiafv.mongodb.net/book_store?retryWrites=true&w=majority"
+}
